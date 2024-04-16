edc34fb2d0bd3c2c464049adb4b123716982b594
florianbgt <florian@gutstickets.org>
2024-04-16 15:39:48 +0200

    wip

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/liveness', (req, res) => {
+  res.send('I am alive!')
+})
+
+app.get('/readiness', (req, res) => {
+  res.send('I am ready!')
 })
 
 app.listen(port, () => {
